f19c9db54f53ca9cc8706560b1ece288446b3bb5
Mani <mchandra@morneaushepell.com>
2021-09-26 22:49:44 -0400

    adding dummy components path in main page

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CSSReset from '@tds/core-css-reset'
 import Home from '../Home';
+import Watches from '../Watches';
+import IPhones from '../IPhones';
 import NotFound from '../NotFound';
 
 const App = () => (
   <Router>
+    <CSSReset />
     <Switch>
-      <Route path="/">
+      <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="/watches">
+        <Watches />
+      </Route>
+      <Route path="/iphones">
+        <IPhones />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   </Router>
