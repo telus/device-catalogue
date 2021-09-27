985d16006dd92ba52d0a7335fab7b778c10976de
Mani <mchandra@morneaushepell.com>
2021-09-27 00:43:43 -0400

    upgrade foot link from text to button brand

--- src/ui/components/Common/FooterLink.js
+++ src/ui/components/Common/FooterLink.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ChevronLink from '@tds/core-chevron-link';
 import Link from './LinkComponent';
+import Button from '@tds/core-button';
 
 const FooterLink = ({ path, text }) => {
   return (
     <div className="footer-link">
-      <Link TDSLink={ChevronLink} to={path}>{text}</Link>
+      <Button variant="brand"><Link TDSLink={ChevronLink} to={path}>{text}</Link></Button>
     </div>
   )
 }
