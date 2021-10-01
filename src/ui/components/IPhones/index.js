526a9f94c362bf4dc5e99978fee03a45717d3457
rajavi <rajavis@zebrarobotics.com>
2021-10-01 12:57:42 -0400

    feat(phones): adding the phones page with device-cards.

--- src/ui/components/IPhones/index.js
+++ src/ui/components/IPhones/index.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Box from "@tds/core-box";
+import FlexGrid from "@tds/core-flex-grid";
+import config from "../../../config.jsx";
+import DeviceCard from "../../helpers/DeviceCard";
+import iphoneImage from "../../../../public/images/iphone.jpg";
 
-const IPhones = () => <div>iPhones</div>;
+const IPhones = () => {
+  const [iphones, setIPhones] = useState([]);
+
+  useEffect(() => {
+    const getIPhones = async () => {
+      try {
+        let iphonesData = await axios.get(`${config.API_ENDPOINT}/iphones`);
+        setIPhones(iphonesData.data.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getIPhones();
+  }, []);
+
+  return (
+    <FlexGrid>
+      <Box vertical={5}>
+        <FlexGrid.Row>
+          {iphones.map((iphone, index) => (
+            <FlexGrid.Col xs={12} md={4} key={index}>
+              <DeviceCard device={iphone} image={iphoneImage} />
+            </FlexGrid.Col>
+          ))}
+        </FlexGrid.Row>
+      </Box>
+    </FlexGrid>
+  );
+};
 
 export default IPhones;
