c222a001f38b33004e8280fd842e83425d5612aa
rajavi <rajavis@zebrarobotics.com>
2021-10-03 10:50:52 -0400

    test(fix): Fixing test to accomodate hooks and axios calls

--- src/ui/__tests__/routes.spec.jsx
+++ src/ui/__tests__/routes.spec.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import wait from "waait";
 import { MemoryRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
 import { mount, configure } from "enzyme";
 import App from "../components/App";
 import Home from "../components/Home";
 import Watches from "../components/Watches";
 import IPhones from "../components/IPhones";
 import NotFound from "../components/NotFound";
+
+const waitForComponentToPaint = async (wrapper) => {
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    wrapper.update();
+  });
+};
 
 describe("Application routes", () => {
   it("should render a Home component", () => {
@@ -25,19 +33,21 @@ describe("Application routes", () => {
         <Watches />
       </MemoryRouter>
     );
-    await wait(100);
+    expect(wrapper.exists).toBeTruthy();
+    await waitForComponentToPaint(wrapper);
     expect(wrapper.find(Watches)).toHaveLength(1);
   });
 
-  // it("should render a iPhones component", async () => {
-  //   const wrapper = mount(
-  //     <MemoryRouter initialEntries={["/iphones"]}>
-  //       <IPhones />
-  //     </MemoryRouter>
-  //   );
-  //   await wait(100);
-  //   expect(wrapper.find(IPhones)).toHaveLength(1);
-  // });
+  it("should render a iPhones component", async () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={["/iphones"]}>
+        <IPhones />
+      </MemoryRouter>
+    );
+    expect(wrapper.exists).toBeTruthy();
+    await waitForComponentToPaint(wrapper);
+    expect(wrapper.find(IPhones)).toHaveLength(1);
+  });
 
   it("should render a 404 page", () => {
     const wrapper = mount(
