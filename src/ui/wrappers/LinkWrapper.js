aa1298fde6f7e6548b1e2913070ed6fa5b7b8bb2
Raaed Kabir <raaed.kabir@gmail.com>
2021-11-16 17:16:53 -0500

    feat(ui): update component to include button tag option

--- src/ui/wrappers/LinkWrapper.js
+++ src/ui/wrappers/LinkWrapper.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import TdsLink from '@tds/core-link';
+import TdsButtonLink from '@tds/core-button-link';
 
-const LinkWrapper = ({ children, ...rest }) => (
-  <TdsLink
-    {...rest}
-    reactRouterLinkComponent={rest.to ? ReactRouterLink : undefined}
-  >
-    {children}
-  </TdsLink>
-);
+const LinkWrapper = ({ children, ...rest }) => {
+  if (rest.button) {
+    return (
+      <TdsButtonLink
+        {...rest}
+        reactRouterLinkComponent={rest.to ? ReactRouterLink : undefined}
+      >
+        {children}
+      </TdsButtonLink>
+    );
+  }
+
+  return (
+    <TdsLink
+      {...rest}
+      reactRouterLinkComponent={rest.to ? ReactRouterLink : undefined}
+    >
+      {children}
+    </TdsLink>
+  );
+};
 
 export default LinkWrapper;
