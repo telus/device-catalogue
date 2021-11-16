03ceecdae15886af44b2937c047e806ae36fa91c
Raaed Kabir <raaed.kabir@gmail.com>
2021-11-16 10:55:39 -0500

    test(ui): add tests to check pages

--- src/ui/__tests__/routes.spec.jsx
+++ src/ui/__tests__/routes.spec.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { mount, configure } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../components/App';
 import Home from '../components/Home';
+import Watches from '../components/Watches';
+import IPhones from '../components/IPhones';
 import NotFound from '../components/NotFound';
 
 describe('Application routes', () => {
@@ -14,5 +16,36 @@ describe('Application routes', () => {
     );
 
     expect(wrapper.find(Home)).toHaveLength(1);
+    expect(wrapper.find(NotFound)).toHaveLength(0);
+  });
+
+  it('should render a Watches component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/watches']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find(Watches)).toHaveLength(1);
+  });
+
+  it('should render a IPhones component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/iphones']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find(IPhones)).toHaveLength(1);
+  });
+
+  it('should render a NotFound component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/404']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find(NotFound)).toHaveLength(1);
   });
 });
