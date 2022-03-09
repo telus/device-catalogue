ca5edd69292d28618bf8570bceacf430fd988633
Reinel Munoz Perez <reinelmp@gmail.com>
2022-03-09 02:21:18 -0500

    Setup TDS

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from '../Home';
-import NotFound from '../NotFound';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CSSReset from "@tds/core-css-reset";
+import Home from "../Home";
+import NotFound from "../NotFound";
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
-  </Router>
+  <>
+    <CSSReset />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
